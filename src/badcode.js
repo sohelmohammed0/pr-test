d4db73f5b35f74edfa216547feb38829f1e159fa
SOHEL MOHAMMED <151991619+sohelmohammed0@users.noreply.github.com>
2026-03-10 10:57:21 +0530

    Test-prod (#4)
    * test: break formatting
    
    * test-1
    
    * fix: remove unused variable
    
    ---------
    
    Co-authored-by: sohelmohammed0 <sohelmohammed88004329@gmail.com>

---
+++ src/badcode.js
@@ -0,0 +1,4 @@
+// src/badFile.js
+export function broken() {
+  return "no coverage"
+}
---
+++ sum.test.js
@@ -0,0 +1,5 @@
+const sum = require('./sum');
+
+test('adds numbers correctly', () => {
+  expect(sum(1, 2)).toBe(3);
+});
